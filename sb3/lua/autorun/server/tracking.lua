6bd1fe75074b961c07bdad4cc17f76941520fab9
LolcatsV2 <LolcatsV2@users.noreply.github.com>
2015-08-01 02:44:28 -0700

    410 status

--- sb3/lua/autorun/server/tracking.lua
+++
@@ -1,6 +0,0 @@
-hook.Add('Initialize','Statistics_Tracking_Spacebuild', function()
-	http.Post('http://catbox.moe/spacebuild/tracking.php', {
-		port = GetConVarString('hostport'),
-		hostname = GetHostName()
-	})
-end)
